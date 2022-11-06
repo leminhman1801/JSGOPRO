68ad235348e4446a92127796df93de1a40ef4af1
leminhman1801 <117287144+leminhman1801@users.noreply.github.com>
2022-11-06 22:50:55 +0700

    Add files via upload

---
+++ toast.js
@@ -0,0 +1,58 @@
+function toast ({
+    title = '',
+    message = '',
+    type = 'info',
+    duration = 3000
+}) {
+    const main = document.getElementById('toast');
+    if (main) {
+        const toast = document.createElement('div');
+
+        // Auto remove toast 
+        const autoRemoveId = setTimeout(function() {
+            main.removeChild(toast);
+        },duration + 1000);
+
+        toast.onclick = function (e) {
+            if (e.target.closest('.toast__close')) {
+                main.removeChild(toast);
+                clearTimeout(autoRemoveId)
+            }
+        }
+
+        const icons = {
+            success: 'fa-regular fa-circle-check',
+            info: 'fa-solid fa-circle-info',
+            warning: 'fa-solid fa-circle-exclamation',
+            error: 'fa-sharp fa-solid fa-bug'
+
+        };
+        const icon = icons [type];
+        const delay = (duration / 1000).toFixed(2);
+        toast.classList.add('toast', `toast--${type}`);
+        toast.style.animation = `slideInLeft ease 0.3s, fadeOut linear 1s ${delay}s forwards`;
+        toast.innerHTML =  `
+        <div class="toast__icon">
+            <i class="${icon}"></i> 
+        </div>
+        <div class="toast__body">
+            <h3 class="toast__title">${title}</h3>
+            <p class="toast_msg">${message}</p>
+        </div>
+        <div class="toast__close">
+            <i class="fa-solid fa-rectangle-xmark"></i>
+        </div>
+    `;
+        main.appendChild(toast);
+
+       
+    }
+    function showSuccessToast () {
+        toast({
+        title: 'Success',
+        message: 'Anyone with access can view your invited visitors',
+        type: 'success',
+        duration: 1000
+        })
+    }
+}
